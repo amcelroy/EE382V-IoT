90f809c49d06a0f38705ad22797946c5c8d61c31
Austin McElroy <mcelroy.austin@gmail.com>
2020-10-04 21:13:36 -0500

    Added flutter app

---
+++ Lab4/IoTApp/flutter_app/android/app/src/main/kotlin/edu/utexas/u382v/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.utexas.u382v.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- Lab4/IoTApp/flutter_app/lib/ee382v_iot.dart
+++ Lab4/IoTApp/flutter_app/lib/ee382v_iot.dart
@@ -1,7 +1,9 @@
 
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_nfc_reader/flutter_nfc_reader.dart';
 import 'package:nfc_manager/nfc_manager.dart';
+import 'package:nfc_manager/platform_tags.dart';
 
 // ignore: camel_case_types
 class EE382V_IoT_NFC extends StatefulWidget{
@@ -12,14 +14,52 @@ class EE382V_IoT_NFC extends StatefulWidget{
 // ignore: camel_case_types
 class _EE382V_IoT_NFC extends State<EE382V_IoT_NFC> {
 
+  Future _nfcThread;
+
+  bool _stopThread = false;
+
+  final int flags = 58; //Hex 0x3A
+
+  void acquireThread(NfcTag tag) {
+    _nfcThread = new Future(() {
+      NfcManager.instance.startSession(
+        onDiscovered: (NfcTag tag) async {
+          NfcV t = NfcV.from(tag);
+          _stopThread = false;
+
+          List<int> data = new List<int>();
+          data.add(flags);
+          data.add(32);
+          data.addAll(tag.data['id']);
+          data.add(0);
+
+          while(_stopThread == false){
+            try{
+              Uint8List response = await t.transceive(data: Uint8List.fromList(data));
+
+              //TODO: Do something with the data
+
+              await Future.delayed(Duration(milliseconds: 75));
+            }on Exception catch (e) {
+              _stopThread = true;
+            }
+          }
+        },
+      );
+    });
+  }
+
   @override
   void initState() async {
     super.initState();
 
-    FlutterNfcReader.read().then((response) {
-      print(response.content);
-    });
+    bool isAvailable = await NfcManager.instance.isAvailable();
 
+    if(isAvailable){
+
+    }else{
+
+    }
   }
 
   @override
