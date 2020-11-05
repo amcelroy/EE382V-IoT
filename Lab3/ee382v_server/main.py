42724787287ef48dc2d2b68079a617047de76c22
Austin McElroy <mcelroy.austin@gmail.com>
2020-11-05 16:06:34 +0000

    Added all the server code in an initial commit

---
+++ Lab3/ee382v_server/main.py
@@ -0,0 +1,45 @@
+import time
+from flask import Flask, jsonify
+from flask import render_template
+from picamera import PiCamera
+import io
+import threading
+from flask_socketio import SocketIO, send, emit
+import queue
+
+stream = io.BytesIO();
+app = Flask(__name__, static_url_path='/static')
+socketio = SocketIO(app)
+camera = PiCamera()
+
+@socketio.on('message')
+def message_handler(message):
+    print(message)
+    if message == 'processed':
+        socketio.emit('frame', q.get())
+
+def camera_thread(q):
+    camera.resolution = (1080, 720)
+    camera.start_preview()
+    time.sleep(2)
+    count = 0
+    for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True, quality=10):
+        num_bytes = stream.tell()
+        stream.seek(0)
+        q.put(stream.read(num_bytes))
+        stream.seek(0)
+        stream.truncate()
+
+@app.route('/snapshot')
+def snapshot():
+    return q.get()
+    #return jsonify({ 'jpeg' : q.get() })
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    q = queue.Queue(maxsize=2)
+    t1 = threading.Thread(target = camera_thread, args=(q,)).start()
+    socketio.run(app, host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
