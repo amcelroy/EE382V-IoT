8ce8c66f66cf10449d8174d90fe96920694dbe7d
JacksonFarley <jackson_farley@baylor.edu>
2020-11-29 15:00:16 -0600

    attempting to work on a periodic client

--- Lab3/ee382v_client/client.py
+++ Lab3/ee382v_client/client.py
@@ -1,9 +1,13 @@
 import socketio
 import time
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from imagecodecs import jpeg_decode
 
+sio = socketio.Client(logger=False, engineio_logger=False, ssl_verify = False)
 
-
-sio = socketio.Client(logger=True, engineio_logger=True)
+start_time = time.time_ns() 
 
 @sio.event
 def connect():
@@ -22,16 +26,35 @@ def disconnect():
 
 @sio.on('frame')
 def on_frame(image):
-    print('I recieved a frame')
+    #print('I recieved a frame')
+    print('Received frame at time %f ms'% ((time.time_ns() - start_time)/1000000))
+    #print(image)
+    #plt.figure()
+    
+    #with open('image_data.pkl', 'wb') as output:
+    #    pickle.dump(image, output, pickle.HIGHEST_PROTOCOL)
+    #    exit()
+
+    my_im = jpeg_decode(image)
+    print("done")
+
+    #plt.imshow(my_im)
+    #plt.savefig('output.jpeg')
+    #print(image.dtype())
     #somehow do something with the image
     #display(image)
 
+sio.connect('https://amcelroy.dyndns.org')
 
+print('sending processed message at time %d ms' % ((time.time_ns() - start_time)/1000000))
 
-sio.connect('http://amcelroy.dyndns.org')
-
-print('sending processed message')
 sio.emit('message','processed')
 
-sio.wait()
+sio.sleep(10)
+
+print('sending processed message at time %d ms' % ((time.time_ns() - start_time)/1000000))
+
+sio.emit('message','processed')
+
+sio.disconnect()
 
