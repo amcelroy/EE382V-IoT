ff74435913ecea4c5a5200ca7b15bfb0b2cba57e
JacksonFarley <jackson_farley@baylor.edu>
2020-11-29 20:21:42 -0600

    working inference client and modified repeating client

--- Lab3/ee382v_client/client.py
+++ Lab3/ee382v_client/client.py
@@ -8,6 +8,16 @@ from imagecodecs import jpeg_decode
 sio = socketio.Client(logger=False, engineio_logger=False, ssl_verify = False)
 
 start_time = time.time_ns() 
+output_filename = 'output0.jpg'
+
+def save_myimage(my_im):
+    plt.imshow(my_im)
+    plt.axis('off')
+    
+    print(output_filename)
+    plt.savefig(output_filename, bbox_inches=0)
+    print("done")
+
 
 @sio.event
 def connect():
@@ -24,10 +34,29 @@ def my_message(data):
 def disconnect():
     print('disconnected from server')
 
-@sio.on('frame')
-def on_frame(image):
+
+#@sio.event
+#def snapshot(image):
+#    print('Received snapshot at time %f s'% ((time.time_ns() - start_time)/1000000000))
+
+#    my_im = jpeg_decode(image)
+#    save_myimage(my_im)
+#    print("done")
+
+@sio.event
+def snapshot(image):
+    print('Received snapshot at time %f s'% ((time.time_ns() - start_time)/1000000000))
+    print('Received an image of '+str(type(image)))
+    sio.emit('labels',data=image)
+
+    #sio.emit('labels',im_jpeg)
+    print("done")
+
+
+#@sio.on('frame')
+#def on_frame(image):
     #print('I recieved a frame')
-    print('Received frame at time %f ms'% ((time.time_ns() - start_time)/1000000))
+    #print('Received frame at time %f ms'% ((time.time_ns() - start_time)/1000000))
     #print(image)
     #plt.figure()
     
@@ -35,8 +64,8 @@ def on_frame(image):
     #    pickle.dump(image, output, pickle.HIGHEST_PROTOCOL)
     #    exit()
 
-    my_im = jpeg_decode(image)
-    print("done")
+    #my_im = jpeg_decode(image)
+    #print("done")
 
     #plt.imshow(my_im)
     #plt.savefig('output.jpeg')
@@ -44,17 +73,24 @@ def on_frame(image):
     #somehow do something with the image
     #display(image)
 
+
 sio.connect('https://amcelroy.dyndns.org')
 
-print('sending processed message at time %d ms' % ((time.time_ns() - start_time)/1000000))
+print('sending processed message at time %f s' % ((time.time_ns() - start_time)/1000000000))
 
-sio.emit('message','processed')
+sio.emit('message',data="serverGetFrame")
 
-sio.sleep(10)
+sio.sleep(5)
+output_filename = 'output1.jpg'
+print('sending processed message at time %f s' % ((time.time_ns() - start_time)/1000000000))
 
-print('sending processed message at time %d ms' % ((time.time_ns() - start_time)/1000000))
+sio.emit('message',data="serverGetFrame")
 
-sio.emit('message','processed')
+sio.sleep(5)
+
+while(1):
+    sio.emit('message', data="serverGetFrame")
+    sio.sleep(5)
 
 sio.disconnect()
-
+exit()
